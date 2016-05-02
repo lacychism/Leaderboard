9e136f1a83710d39fcebd56dd6fb9ea19c55d799
Justin Frasier <justin_frasier@me.com>
2016-05-02 10:04:55 -0500

    All most done having trouble posting to the server

--- Project/ObtainDatabase.pas
+++ Project/ObtainDatabase.pas
@@ -9,13 +9,12 @@ uses
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, Xml.xmldom,
   Xml.XMLIntf, Xml.XMLDoc, FMX.Controls.Presentation, FMX.Edit, FMX.StdCtrls,IdHTTP,
   IdBaseComponent, IdComponent, IdTCPConnection, IdTCPClient, FMX.ScrollBox,net,
-  FMX.Memo;
+  FMX.Memo, xmlLoadSaveAndroid;
 
 type
 
   TForm1 = class(TForm)
-    ScheduleXML: TXMLDocument;
-    TeamCodeXML: TXMLDocument;
+
     Button1: TButton;
     Memo1: TMemo;
     procedure FormCreate(Sender: TObject);
@@ -29,8 +28,6 @@ type
 var
   Form1: TForm1;
   net: TIdHTTP;
-  http: TIdCustomHTTP;
-
 implementation
 
 {$R *.fmx}
@@ -41,73 +38,59 @@ implementation
 {$R *.Windows.fmx MSWINDOWS}
 
 procedure TForm1.Button1Click(Sender: TObject);
-var
-  server: string;
-  users: string;
-  login: string;
-  code: Integer;
-  userXML: TXMLDocument;
 
+var
+  TeamCodeXML: TXMLDocument;
 
 begin
-  server:= 'http://173.217.40.147:8080/Test/';
-  users:= 'user.xml';
-  login:= 'login.xml';
-  try
-     try
-      http:=TIdCustomHTTP.Create();
-      http.Request.ContentType:= 'text/xml; charset=utf-8';
-      http.Request.ContentEncoding:= 'utf-8';
-      http.Get(server+users);
-      Memo1.Lines.Add('Loaded users');
-     except
-       on E: Exception do
-       begin
-             Memo1.Lines.Add('Did Not Load users');
-            try
-              userXML:=TXMLDocument.Create(nil);
-              http.Post(server+users,userXML.XML);
-              Memo1.Lines.Add('Posted users');
-            except
-               on E: Exception do
-                  Memo1.Lines.Add('Did Not Post users');
-            end;
-       end;
 
-     end;
-
-  finally
-     http.Disconnect();
-     http.Destroy();
-  end;
-
-
-
-
-  ShowMessage('Done');
+  TeamCodeXML:= TXMLDocument.Create(nil);
+  TeamCodeXML.LoadFromXML(HttpGet(getServerIP+'team.xml'));
+  SaveXMLFile(TeamCodeXML, 'team.xml');
+  upLoadFile(LoadXMLFile('team.xml'),getServerIP+'team.xml');
 
 end;
 
 procedure TForm1.FormCreate(Sender: TObject);
  var
-  buffer: String;
-  team: string;
-  schedule: string;
+  UsersXML: TXMLDocument;
+  TeamCodeXML: TXMLDocument;
+  ScheduleXML: TXMLDocument;
+  LoginXML: TXMLDocument;
   server: string;
-  test: string;
 
 
 begin
-  server:= 'http://173.217.40.147:8080/Test/';
-  team:=  'team.xml';
-  schedule:=  'schedule.xml';
-  TeamCodeXML:= TXMLDocument.Create(nil);
-  ScheduleXML:= TXMLDocument.Create(nil);
+  try
+    try
+        TeamCodeXML:= TXMLDocument.Create(nil);
+        ScheduleXML:= TXMLDocument.Create(nil);
+        UsersXML:= TXMLDocument.Create(nil);
+        LoginXML:= TXMLDocument.Create(nil);
+        server:= getServerIP;
+        Memo1.Lines.Add(getServerIP);
 
-  TeamCodeXML.LoadFromXML(HttpGet(server+team));
-  Memo1.Lines.Add('Loaded Team');
-  ScheduleXML.LoadFromXML(HttpGet(server+team));
-  Memo1.Lines.Add('Loaded Schedule');
+        TeamCodeXML.LoadFromXML(HttpGet(server+'team.xml'));
+        Memo1.Lines.Add('Loaded Team');
+        ScheduleXML.LoadFromXML(HttpGet(server+'schedule.xml'));
+        Memo1.Lines.Add('Loaded Schedule');
+        UsersXML.LoadFromXML(HttpGet(server+'users.xml'));
+        Memo1.Lines.Add('Loaded Users');
+        LoginXML.LoadFromXML(HttpGet(server+'login.xml'));
+        Memo1.Lines.Add('Loaded Login');
+
+    except
+       on E: Exception do
+       begin
+        ShowMessage('something fucked up');
+       end;
+
+    end;
+
+  finally
+
+  end;
+
 
 
 end;
